16913fb2e95c0c2790080537a8a6dc3a76e0e0b1
AMIN <aminaminul223@gmail.com>
2025-12-24 17:36:06 +0600

    Created Instructor index page

---
+++ SmartQuizAssessmentSystem/Controllers/InstructorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartQuizAssessmentSystem.Controllers
+{
+    [Authorize (Roles = "Instructor")]
+    public class InstructorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
