062d24d1b50cc7c1ff5a40d20f952f4bed4445f2
AMIN <aminaminul223@gmail.com>
2025-12-26 10:11:14 +0600

    Updated ViewModels to Configure Registration

--- QuizModel/ViewModels/RegisterViewModel.cs
+++ QuizModel/ViewModels/RegisterViewModel.cs
@@ -5,25 +5,26 @@ namespace QuizSystemModel.ViewModels
     public class RegisterViewModel
     {
         [Required(ErrorMessage = "First Name is required.")]
-        public string FirstName { get; set; }
+        public required string FirstName { get; set; }
         
         [Required(ErrorMessage = "First Name is required.")]
-        public string LastName { get; set; }
+        public required string LastName { get; set; }
 
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
         [DataType(DataType.Password)]
         [Compare("ConfirmPassword", ErrorMessage = "Password does not match.")]
-        public string Password { get; set; }
+        public required string Password { get; set; }
 
         [Required(ErrorMessage = "Confirm Password is required.")]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
-        public string ConfirmPassword { get; set; }
+        [Compare("ConfirmPassword", ErrorMessage = "Password does not match.")]
+        public required string ConfirmPassword { get; set; }
         [Required]
         public string Role { get; set; }
 
